6d746ff65b6197d616ebe28736f0a42b7a7b0fde
Mike Partelow <github@mikepartelow.com>
2024-04-10 22:14:32 -0700

    main

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"mp/huffer/pkg/huffman"
+	"os"
+)
 
+func main() {
+	const message0 = "For any given degree of noise contamination of a communication channel, it is possible (in theory) to communicate discrete data (digital information) nearly error-free up to a computable maximum rate through the channel."
+
+	encoding, table := huffman.Encode([]rune(message0))
+
+	message1, err := huffman.Decode(encoding, len(message0), table)
+	if err != nil {
+		fmt.Println("error:", err.Error())
+		os.Exit(1)
+	}
+
+	if message0 != string(message1) {
+		fmt.Println("error: message0 != message1")
+		os.Exit(1)
+	}
+
+	fmt.Println("🤙")
 }
